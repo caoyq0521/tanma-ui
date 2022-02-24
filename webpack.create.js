790ae495eda67621f18c563a7fb5ebbcfdb74a47
曹永强 <caoyongqiang@aihujing.com>
2022-02-24 08:42:10 +0800

    更改webpack配置

--- webpack.create.js
+++ webpack.create.js
@@ -3,7 +3,6 @@ const CreateDirStructure = require('./plugins/createDirStructure');
 
 module.exports = {
   mode: 'development',
-  entry: resolve(__dirname, 'src/index.js'),
   plugins:[
     new CreateDirStructure({
       name: 'button',
