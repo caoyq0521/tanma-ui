72529089bfaee6af87574d4a4217ea2521c7b39e
曹永强 <caoyongqiang@aihujing.com>
2022-03-10 10:12:04 +0800

    完善文档内容

--- plugins/createDirStructure/createFileContent.js
+++ plugins/createDirStructure/createFileContent.js
@@ -50,7 +50,28 @@ ${comptTemplate}
 
 <demo-code>./demo/index.vue</demo-code>
 
-### Props`
+### Props
+
+### Events
+
+### Slots
+
+### 样式变量
+
+#### Less 变量
+
+组件提供了下列 Less 变量，可用于自定义样式，使用方法请参考[定制主题 less](#/theme)。
+
+名称 | 默认值 | 描述
+-- | -- | --
+
+#### Css 变量
+
+组件提供了下列 Css 变量，可用于自定义样式，使用方法请参考[定制主题 css](#/theme2)。
+
+名称 | 默认值 | 描述
+-- | -- | --
+`
 }
 
 function createStyleContent(name) {
