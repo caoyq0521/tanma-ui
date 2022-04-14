6f1a28679e3a3a03c8e56b650f764a45f4f9fa58
qiaoleicai <luffy0101@163.com>
2022-04-14 15:09:49 +0800

    collapseText添加动画

--- src/collapse-text/collapse-text.vue
+++ src/collapse-text/collapse-text.vue
@@ -2,7 +2,7 @@
 <div>
   <div class="tm-collapse-text" ref="collapseText">
     <div v-if="!(showPopover && showPopoverJudge)">
-      <span class="tm-collapse-text__content" :style="expandStyle">{{ (text === null || text === undefined || text === '') ? '--' : text }}</span>
+      <div class="tm-collapse-text__content" ref="collapseTextContent" :style="expandStyle">{{ (text === null || text === undefined || text === '') ? '--' : text }}</div>
       <div class="tm-collapse-text__action" v-if="showButton && showButtonJudge">
         <span
           v-if="!showFull"
@@ -28,15 +28,6 @@
       v-title="text">
       {{ text }}
     </div>
-    <!-- <el-popover
-      v-else
-      :placement="popoverPlace"
-      trigger="hover">
-      <div class="tm-collapse-text__popover">
-        {{ text }}
-      </div>
-      <span class="tm-collapse-text__content" :style="expandStyle" slot="reference">{{ text }}</span>
-    </el-popover> -->
   </div>
 </div>
 </template>
@@ -83,19 +74,16 @@
       showPopover: {
         type: Boolean,
         default: false
-      },
-      // popover展示位置
-      // popoverPlace: {
-      //   type: String,
-      //   default: 'bottom'
-      // }
+      }
   },
   data () {
     return {
       showFull: false, // 是否展示全文本
       expandStyle: '',
       showButtonJudge: false, // 判断是否需要折叠展示按钮
-      showPopoverJudge: false // 判断是否需要折叠展示popover
+      showPopoverJudge: false, // 判断是否需要折叠展示popover
+      minHeight:'',
+      maxHeight: ''
     }
   },
   watch: {
@@ -110,6 +98,7 @@
     showFullFn (value) {
       this.expandStyle = value ? '' : `display: -webkit-box;word-break: break-all;-webkit-line-clamp: ${this.lines};-webkit-box-orient: vertical;text-overflow: ellipsis;overflow: hidden;`
       this.showFull = value
+      this.$refs.collapseTextContent.style.height = value ? `${this.maxHeight}px` : `${this.minHeight}px`;
     },
     judgeExpand () {
       this.showButtonJudge = false
@@ -121,6 +110,7 @@
         const collapseTextLineHeight = parseFloat(collapseTextStyle.lineHeight === 'normal' ? parseFloat(collapseTextStyle.fontSize) * 1.2 : collapseTextStyle.lineHeight) // 获取行高
         // 计算行高
         const rects = Math.ceil(collapseTextHeight / collapseTextLineHeight)
+        this.maxHeight = parseFloat(window.getComputedStyle(this.$refs.collapseTextContent).height)
         if (rects <= lines) { // 不需要折叠展示
           this.showButtonJudge = false
           this.showPopoverJudge = false
@@ -128,6 +118,8 @@
           this.showButtonJudge = true
           this.showPopoverJudge = true
           this.expandStyle = `display: -webkit-box;word-break: break-all;-webkit-line-clamp: ${this.lines};-webkit-box-orient: vertical;text-overflow: ellipsis;overflow: hidden;`
+          this.minHeight = collapseTextLineHeight
+          this.$refs.collapseTextContent.style.height = `${this.minHeight}px`
         }
       })
     }
