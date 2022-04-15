5c937c2878f01e8996686c2e7a2954131dbc9205
赵子玮 <zhaoziwei@tanmarket.cn>
2022-04-15 16:50:10 +0800

    button-tabs 组件补充测试用例

--- src/button-tabs/test/index.spec.js
+++ src/button-tabs/test/index.spec.js
@@ -6,5 +6,24 @@ describe('tm-button-tabs', () => {
     const wrapper = mount(TmButtonTabs);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('create button-tabs', () => {
+    const wrapper = mount(TmButtonTabs);
+
+    expect(wrapper.find('.tm-button-tabs').exists()).toBe(true);
+  });
+
+  it('whether type of button', async () => {
+    const wrapper = mount(TmButtonTabs);
+
+    await wrapper.setProps({ type: 'step' });
+    expect(wrapper.find('.tm-button-tabs__step').exists()).toBe(true);
+
+    await wrapper.setProps({ type: 'certainty' });
+    expect(wrapper.find('.tm-button-tabs__certainty').exists()).toBe(true);
+
+  });
+
 })
+
   
